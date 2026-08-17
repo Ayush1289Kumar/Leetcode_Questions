817682cf5e45cb540e131dc6d48ef8fc25060e49
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-08-17 11:47:44 +0530

    Time: 3 ms (57.20%) | Memory: 19.3 MB (37.35%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        balloon = {'b':1,'a':1,'l':2,'o':2,'n':1}
+        map = {}
+
+        for c in text:
+            map[c] = map.get(c,0)+1
+        
+        times = float("inf")
+
+        for c in balloon:
+            if c not in map:
+                return 0
+            
+            attempt = map[c]//balloon[c]
+
+            times = min(times,attempt)
+        
+        return times
