3335d709837c1fb35afa95b7228b56495d8395bd
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-08-17 16:22:12 +0530

    Time: 3 ms (46.09%) | Memory: 19.3 MB (67.47%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        freq = {}
+        for c in s:
+            freq[c] = freq.get(c,0)+1
+        
+        ans = 0
+        hasOdd = False
+        
+        for value in freq.values():
+            if value%2 == 0:
+                ans+=value
+            else:
+                ans+=value-1
+                hasOdd = True
+        
+        if hasOdd:
+            ans+=1
+
+        return ans
