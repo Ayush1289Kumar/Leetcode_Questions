29d45f485bbd5c7d9abf0823f2eb02f133418ff9
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-08-18 22:41:45 +0530

    Time: 183 ms (67.71%) | Memory: 26 MB (13.44%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,22 +1,22 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        n = len(fruits)
         low = 0
+        max_fruits = 0
         basket ={}
-        ans = -1
-        
-        for high in range(len(fruits)):
+
+        for high in range(n):
             basket[fruits[high]] = basket.get(fruits[high],0)+1
-            
+
             while (len(basket) > 2):
                 basket[fruits[low]]-=1
-                
+
                 if basket[fruits[low]] == 0:
                     del basket[fruits[low]]
+                
                 low+=1
             
-            if len(basket) <= 2:
-                window_length = high-low+1
-                if ans<window_length:
-                    ans = window_length
-            
-        return ans
+            curr_fruits = high-low+1
+            max_fruits = max(curr_fruits,max_fruits)
+        
+        return max_fruits 
