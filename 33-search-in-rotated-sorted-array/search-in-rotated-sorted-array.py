c8098cedd63d5af55d6d764dfc67c1f01b3f7e10
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-08-27 23:17:31 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (38.86%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -0,0 +1,48 @@
+class Solution:
+
+    def minV_idx(self,nums):
+        n = len(nums)
+        min_idx = -1
+        start,end = 0,n-1
+
+        while(start <=end):
+            mid = (start+end)//2
+
+            if (nums[mid] > nums[n-1]):
+                start = mid+1
+            else:
+                min_idx = mid
+                end = mid -1
+        return min_idx
+    
+    def binarySearch(self,s,e,nums,target):
+        start, end = s, e
+
+        while (start <= end):
+            mid = (start+end)//2
+
+            if nums[mid] < target:
+                start = mid+1
+            
+            elif nums[mid] > target:
+                end = mid-1
+            
+            else:
+                return mid
+        
+        return -1
+
+    def search(self, nums: List[int], target: int) -> int:
+        # First we will find the minimum index and then we will break the array in two parts, and we will apply binary search in that parts.
+        s1, e1 = 0, self.minV_idx(nums)
+        s2, e2 = self.minV_idx(nums), len(nums)-1
+
+        ans = -1
+        ans = self.binarySearch(s1,e1,nums,target)
+
+        if ans==-1:
+            ans = self.binarySearch(s2,e2,nums,target)
+
+        return ans
+        
+        
