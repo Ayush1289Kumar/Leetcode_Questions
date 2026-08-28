c83036ab6e02630de03829a79f8bff47e119c4bb
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-08-28 22:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (10.45%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,48 +1,37 @@
 class Solution:
-
-    def minV_idx(self,nums):
-        min_idx = 0
-        start , end = 0, len(nums) -1
-
-        while(start<=end):
-            mid = (start+end)//2
-
-            if (nums[mid] < nums[min_idx]):
-                min_idx = mid
-                end = mid-1
-            else:
-                start = mid+1
-        
-        return min_idx
-    
-    def binarySearch(self,start,end,nums,target):
-        while (start <= end):
-            mid = (start+end)//2
-
-            if nums[mid] < target:
-                start = mid+1
-            
-            elif nums[mid] > target:
-                end = mid-1
-            
-            else:
-                return mid
-        
-        return -1
-
     def search(self, nums: List[int], target: int) -> int:
-        # First we will find the minimum index and then we will break the array in two parts, and we will apply binary search in that parts.
-        """
-        binarySearch(0,min_idx)
-        binarySearch(min_idx,n-1)
-        
-        """
-        ans = -1
-        ans = self.binarySearch(0,self.minV_idx(nums),nums,target)
+        n = len(nums)
+        start, end = 0,n-1
 
-        if ans==-1:
-            ans = self.binarySearch(self.minV_idx(nums),len(nums)-1,nums,target)
+        while(start <= end):
+            mid = (start+end)//2
 
-        return ans
-        
-        
+            # Part 1 
+            if (nums[mid] > nums[n-1]):
+                if nums[mid] == target:
+                    return mid
+                
+                elif nums[mid] < target:
+                    start = mid+1
+                
+                else:
+                    if nums[0] <= target:
+                        end = mid-1
+                    else:
+                        start = mid+1
+            
+            # Part 2
+            else:
+                if nums[mid] == target:
+                    return mid
+                
+                elif nums[mid] > target:
+                    end = mid-1
+                
+                else:
+                    if nums[n-1] >= target:
+                        start = mid+1
+                    else:
+                        end = mid-1
+            
+        return -1
