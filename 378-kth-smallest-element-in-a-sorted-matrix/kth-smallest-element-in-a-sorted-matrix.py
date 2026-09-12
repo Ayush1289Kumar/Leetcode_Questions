4f9614fa5b1506e1a45d40528a2be96d1d23b2c0
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-09-12 10:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (69.03%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -1,31 +1,31 @@
 class Solution:
-    def LessThan(self,matrix,m,n,guess):
-        row = m-1
+    def isLessThan(self,matrix,m,n,guess,k):
+        row = m - 1
         col = 0
         cnt = 0
-        while (row>=0  and col < n):
+
+        while (row>= 0 and col < n):
             if matrix[row][col] <= guess:
-                cnt+= row+1
+                cnt += row+1
                 col+=1
             else:
                 row-=1
         
-        return cnt
-
+        return cnt >= k
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         m = len(matrix)
         n = len(matrix[0])
         low = matrix[0][0]
-        high = matrix[m - 1][n - 1]
+        high = matrix[m-1][n-1]
         ans = -1
 
-        while (low<=high):
+        while (low<= high):
             guess = (low+high)//2
-            num = self.LessThan(matrix,m,n,guess)
-            if num < k:
-                low = guess+1
-            else:
+
+            if self.isLessThan(matrix,m,n,guess,k):
                 ans = guess
                 high = guess - 1
+            else:
+                low = guess + 1
         
         return ans
