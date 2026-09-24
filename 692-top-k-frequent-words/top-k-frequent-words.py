68cf9c198336e53aa1e89e5a1da51c6cea04a9b7
Ayush Kumar <ayushkumarpro1289@gmail.com>
2026-09-24 12:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (74.35%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,34 +1,41 @@
-import heapq as h
+import heapq
 
-class Pairs:
-    def __init__(self,first,second):
-        self.first = first
-        self.second = second
-    
-    def __lt__(self,other):
-        if self.first != other.first:
-            return self.first < other.first
-        return self.second > other.second
+
+class Pair:
+    def __init__(self, freq, word):
+        self.freq = freq
+        self.word = word
+
+    def __lt__(self, other):
+        if self.freq != other.freq:
+            return self.freq < other.freq
+
+        return self.word > other.word
+
 
 class Solution:
     def topKFrequent(self, words: list[str], k: int) -> list[str]:
+
+        freq = {}
+
+        for word in words:
+            freq[word] = freq.get(word, 0) + 1
+
         heap = []
-        map = {}
+
+        for word, count in freq.items():
+
+            if len(heap) < k:
+                heapq.heappush(heap, Pair(count, word))
+
+            elif count > heap[0].freq or (
+                count == heap[0].freq and word < heap[0].word
+            ):
+                heapq.heapreplace(heap, Pair(count, word))
+
         ans = []
 
-        for i in words:
-            map[i] = map.get(i,0)+1    # element -> frequency
-        
-        for key,value in map.items():
-            if len(heap) < k:
-                h.heappush(heap,Pairs(value,key))
-
-            elif (heap[0].first < value or
-                  (heap[0].first == value and heap[0].second > key)):
-                h.heappop(heap)
-                h.heappush(heap,Pairs(value,key))
-        
         while heap:
-            ans.append(h.heappop(heap).second)
-        
+            ans.append(heapq.heappop(heap).word)
+
         return ans[::-1]
